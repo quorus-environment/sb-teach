8b16549c5fb35fa4f4beb43d928cf84783610896
AricsyGIT <aricsybsn@gmail.com>
2023-04-15 13:14:34 +0500

    feat: updated user storage

---
+++ src/shared/stores/user/lib/user-store.ts
@@ -0,0 +1,48 @@
+import { create } from "zustand"
+import { immer } from "zustand/middleware/immer"
+import { devtools, persist } from 'zustand/middleware'
+
+import {TUser} from "../model/interface";
+type Actions = {
+    register: (user: any) => void;
+    logout: () => void;
+    cleanErrors: () => void;
+    login: (user: any) => void
+};
+
+
+export const useAuthStore = create<TUser & Actions>()(
+    immer(
+        (set) => ({
+            isAuth: false,
+            error: null,
+            loading: false,
+            role: null,
+            _id: null,
+            register: async (user) => {
+                const data = fetch("http://localhost:3000", user)
+                set({loading: true})
+                localStorage.setItem("accessToken", '' )
+                try {
+                    set(() => ({
+                        isAuth: true,
+                    }));
+                    set({loading: false})
+                } catch (error) {
+                    set(() => ({ error: "", loading: false}));
+                }
+            },
+            login: async(user) => {
+                const data = fetch("http://localhost:3000", user)
+                try {
+                    set(() => ({
+                        isAuth: true,
+                    }));
+                } catch (error) {
+                    set(() => ({ error: "" }));
+                }
+            } ,
+            logout: () => set(() => ({})),
+            cleanErrors: () => set(() => ({ error: null })),
+        })
+    ));
---
+++ src/shared/stores/user/model/interface.ts
@@ -0,0 +1,7 @@
+export type TUser = {
+    _id: string | null,
+    role: string | null,
+    isAuth: boolean,
+    error: string | null
+    loading: boolean,
+}
