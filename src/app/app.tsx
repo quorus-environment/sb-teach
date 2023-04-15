1d6f94fc5f52cf02a3901330f50d6f65f9925d74
AricsyGIT <aricsybsn@gmail.com>
2023-04-15 15:25:16 +0500

    made protected route

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,29 @@
 import './app.css'
 import {Auth} from "../pages/auth/auth";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 
 const App = () => {
-  return (
-    <div className="app">
-          <Auth/>
-    </div>
-  )
+    const token = localStorage.getItem("token")
+    if (!token) {
+        return (
+            <div id="app">
+                <Routes>
+                    <Route path="/sign-in" element={<Auth />}></Route>
+                    <Route path="*" element={<Navigate to="/sign-in" />}></Route>
+                </Routes>
+            </div>
+        );
+    }
+
+    return (
+        <div id="app">
+            <Routes>
+                <Route path="/" element={<div>content</div>}></Route>
+                <Route path="*" element={<Navigate to={"/"} />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App
--- src/shared/stores/user/lib/user-store.ts
+++ src/shared/stores/user/lib/user-store.ts
@@ -1,62 +1,63 @@
 import { create } from "zustand"
 import { immer } from "zustand/middleware/immer"
-import { devtools, persist } from 'zustand/middleware'
+import { devtools, persist } from "zustand/middleware"
 
-import {TUserStore} from "../model/interface";
-import {TSignIn, TSignUp} from "../../../services/auth-model";
-import {AuthService} from "../../../services/auth-service";
+import { TUserStore } from "../model/interface"
+import { TSignIn, TSignUp } from "../../../services/auth-model"
+import { AuthService } from "../../../services/auth-service"
 type Actions = {
-    register: (user: TSignUp) => void;
-    logout: () => void;
-    cleanErrors: () => void;
-    login: (user: TSignIn) => void
-};
+  register: (user: TSignUp) => void
+  logout: () => void
+  cleanErrors: () => void
+  login: (user: TSignIn) => void
+}
 
 const initialState = {
-    isAuth: false,
-    error: null,
-    user: null,
-    loading: false,
+  isAuth: false,
+  error: null,
+  user: null,
+  loading: false,
 }
 
 export const useAuthStore = create<TUserStore & Actions>()(
-    devtools(immer(
-        (set) => ({
-            ...initialState,
-            register: async (user) => {
-                set({loading: true})
-                const {data} = await AuthService.register(user)
-                localStorage.setItem("accessToken", data["access_token"] )
-                try {
-                    set(() => ({
-                        isAuth: true,
-                        user: data.user
-                    }));
-                    set({loading: false})
-                } catch (error) {
-                    set(() => ({ error: "", loading: false}));
-                }
-            },
+  devtools(
+    immer((set) => ({
+      ...initialState,
+      register: async (user) => {
+        set({ loading: true })
+        const { data } = await AuthService.register(user)
+        localStorage.setItem("accessToken", data["access_token"])
+        try {
+          set(() => ({
+            isAuth: true,
+            user: data.user,
+          }))
+          set({ loading: false })
+        } catch (error) {
+          set(() => ({ error: "", loading: false }))
+        }
+      },
 
-            login: async(user: TSignIn) => {
-                const {data} = await AuthService.login(user)
-                localStorage.setItem("accessToken", data["access_token"])
-                try {
-                    set(() => ({
-                        isAuth: true,
-                        user: data.user
-                    }));
-                } catch (error) {
-                    set(() => ({ error: "" }));
-                }
-            } ,
-            logout: () => {
-                localStorage.setItem("accessToken", '')
-                set(() => ({
-                    ...initialState
-                }))
-            },
+      login: async (user: TSignIn) => {
+        const { data } = await AuthService.login(user)
+        localStorage.setItem("accessToken", data["access_token"])
+        try {
+          set(() => ({
+            isAuth: true,
+            user: data.user,
+          }))
+        } catch (error) {
+          set(() => ({ error: "" }))
+        }
+      },
+      logout: () => {
+        localStorage.setItem("accessToken", "")
+        set(() => ({
+          ...initialState,
+        }))
+      },
 
-            cleanErrors: () => set(() => ({ error: null })),
-        })
-    )));
+      cleanErrors: () => set(() => ({ error: null })),
+    })),
+  ),
+)
